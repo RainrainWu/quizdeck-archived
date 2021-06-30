75945ff7fe88642ec33d7473cb74814dc067d5ad
RainrainWu <s0958334772@gmail.com>
2021-07-01 00:14:20 +0800

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,9 +5,16 @@ resource "aws_lambda_function" "quizdeck_lab" {
     aws_cloudwatch_log_group.quizdeck_lab,
   ]
 
-  function_name = "quizdeck_lab"
+  function_name = var.LAMBDA_QUIZDECK_LAB_FUNCTION_NAME
   role          = aws_iam_role.quizdeck_lab.arn
   package_type  = "Image"
   image_uri     = "${var.AWS_ACCOUNT_ID}.dkr.ecr.${var.AWS_REGION}.amazonaws.com/${var.ECR_QUIZDECK_LAB_REGISTRY_NAME}:latest"
   timeout       = 60
+}
+
+resource "aws_lambda_permission" "quizdeck_lab" {
+  action        = "lambda:InvokeFunction"
+  function_name = var.LAMBDA_QUIZDECK_LAB_FUNCTION_NAME
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.quizdeck.execution_arn}/*/*/*"
 }
