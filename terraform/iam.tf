e2dce467d96288f10ad2ebd3972a2b8beb17d760
RainrainWu <s0958334772@gmail.com>
2021-06-30 20:30:14 +0800

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -31,11 +31,6 @@ resource "aws_iam_policy" "quizdeck_lab" {
   "Statement": [
     {
       "Effect": "Allow",
-      "Action": "logs:CreateLogGroup",
-      "Resource": "arn:aws:logs:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:*"
-    },
-    {
-      "Effect": "Allow",
       "Action": [
         "logs:CreateLogStream",
         "logs:PutLogEvents"
