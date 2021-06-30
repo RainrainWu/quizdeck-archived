7eb214544fb3ac975c01e403b9d51315fc35b075
RainrainWu <s0958334772@gmail.com>
2021-06-30 19:58:52 +0800

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,17 +4,17 @@ resource "aws_iam_role" "quizdeck_lab" {
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Sid": ""
-      }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Sid": ""
+    }
+  ]
 }
 EOF
 }
@@ -27,7 +27,7 @@ resource "aws_iam_policy" "quizdeck_lab" {
 
   policy = <<EOF
 {
-  "Version": "2021-06-30",
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Effect": "Allow",
@@ -36,7 +36,7 @@ resource "aws_iam_policy" "quizdeck_lab" {
         "logs:PutLogEvents"
       ],
       "Resource": [
-          "arn:aws:logs:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:log-group:/aws/lambda/${var.LAMBDA_QUIZDECK_LAB_FUNCTION_NAME}:*"
+        "arn:aws:logs:${var.AWS_REGION}:${var.AWS_ACCOUNT_ID}:log-group:/aws/lambda/${var.LAMBDA_QUIZDECK_LAB_FUNCTION_NAME}:*"
       ]
     }
   ]
