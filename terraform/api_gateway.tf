64fe9c787576e2cbe64f514d6132d95ab88ff2bf
RainrainWu <s0958334772@gmail.com>
2021-06-30 22:44:26 +0800

    feat(provision): add api gateway integration

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -10,8 +10,13 @@ resource "aws_apigatewayv2_integration" "example" {
   integration_uri    = aws_lambda_function.quizdeck.invoke_arn
 }
 
-resource "aws_apigatewayv2_route" "example" {
+resource "aws_apigatewayv2_route" "quizdeck" {
   api_id    = aws_apigatewayv2_api.quizdeck.id
   route_key = "ANY /discord/{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.quizdeck.id}"
+}
+
+resource "aws_apigatewayv2_stage" "quizdeck" {
+  api_id = aws_apigatewayv2_api.quizdeck.id
+  name   = "develop"
 }
